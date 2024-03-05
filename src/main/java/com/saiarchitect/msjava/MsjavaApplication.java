2b1c7968efff8ac57f7445fac4bf48b5ebcaedc9
Kothuru <KS06501@bcbsa.com>
2024-03-05 12:26:32 -0600

    Initial Commit

---
+++ src/main/java/com/saiarchitect/msjava/MsjavaApplication.java
@@ -0,0 +1,13 @@
+package com.saiarchitect.msjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saiarchitect/msjava/MsjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saiarchitect.msjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
